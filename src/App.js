75133e5bf2d2d4bd9a3a3c266ee4f59248318af3
marville001 <mwangimartin1904@gmail.com>
2021-02-01 11:50:39 +0300

    First Commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import Sidebar from "./components/Sidebar";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <div className="row">
+          <div className="col-lg-3">
+            <div className="app__sidebar">
+              <Sidebar/>
+            </div>
+          </div>
+          <div className="col-lg-9 app__main-content">
+            main container
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,51 @@
+import React from "react";
+
+import facebook from "../assets/icons/facebook.svg";
+import github from "../assets/icons/github.svg";
+import instagram from "../assets/icons/instagram.svg";
+import pin from "../assets/icons/pin.svg";
+import tie from "../assets/icons/tie.svg";
+import mightycoder from "../assets/mightycoder.svg";
+
+const Sidebar = () => {
+  return (
+    <div>
+      <img src={mightycoder} alt="avatar" className="sidebar__avatar" />
+      <div className="sidebar__name">
+        Martin <span>Mwangi</span>
+      </div>
+      <div className="sidebar__item sidebar__title">Web Developer</div>
+      <a href="">
+        <div className="sidebar__item">
+          <img src={tie} alt="resume" className="sidebar__icon" /> Download
+          Resume
+        </div>
+      </a>
+      <figure className="sidebar__social-icons">
+        <a href="">
+          <img src={facebook} className="sidebar__icon" alt="facebook" />
+        </a>
+        <a href="">
+          <img src={instagram} className="sidebar__icon" alt="instagram" />
+        </a>
+      </figure>
+      <div className="contact">
+        <div className="sedebar__item">
+          <a href="">
+            <img src={github} alt="" className="sidebar__icon" />
+            github
+          </a>
+        </div>
+        <div className="sidebar__location">
+          <img src={pin} alt="location" className="sidebar__icon" />
+          Murang'a Kenya
+        </div>
+        <div className="sidebr__item">mwangimartin1904@gmail.com</div>
+        <div className="sidebr__item">+254700207054 / +254762075082</div>
+      </div>
+      <div className="sidebar__item sidebar__email">email me</div>
+    </div>
+  );
+};
+
+export default Sidebar;
